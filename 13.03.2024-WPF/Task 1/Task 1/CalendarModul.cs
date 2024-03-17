40baf86e8f551a5efd103e0c71fca88a530109c9
garbich <131876643+garbich@users.noreply.github.com>
2024-03-17 18:42:12 +0200

    Add files via upload

---
+++ 13.03.2024-WPF/Task 1/Task 1/CalendarModul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Task_1
+{
+
+    internal class CalendarModul
+    {
+        public string addDateTolist(string date, List<string> selectedDates)
+        {
+            MainWindow mainWindow = new MainWindow();
+            string selectedDate = date;
+
+            if (!selectedDates.Contains(selectedDate))
+            {               
+                return selectedDate;
+            }
+            return "0";
+        }
+    }
+}
---
+++ 13.03.2024-WPF/Task 1/Task 1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> selectedDates = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {     
+
+          
+            CalendarModul calendarModul = new CalendarModul();
+
+            string date = calendarModul.addDateTolist(ChooseCalendar.SelectedDate.Value.ToString(), selectedDates);
+
+            if (date != "0")
+            {
+                ListBox.Items.Add(date);
+                selectedDates.Add(date);
+            }
+            else
+            {
+                MessageBox.Show("Already reserved");             
+            }
+           
+        }
+    }
+   
+    
+}
